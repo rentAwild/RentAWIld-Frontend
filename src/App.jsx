aa2bcbb06104c85dde544e7c8bd8309c93de4050
André Macedo <asilvamacedo@gmail.com>
2023-01-11 12:12:13 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+/* import { Routes, Route } from "react-router-dom";
+ */
+
+function App() {
+  return <div className="app-body" />;
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+    },
+  },
+});
