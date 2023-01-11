06a71bf10a515d6935553f8a77978d977b8a509b
r8ch <81969279+r8ch@users.noreply.github.com>
2023-01-11 12:29:45 +0000

    create route for login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
-/* import { Routes, Route } from "react-router-dom";
- */
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <div className="app-body" />;
+  return (
+    <div className="app-body">
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
