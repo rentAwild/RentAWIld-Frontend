affd6dad5dc3b50fd895d1a484b1e541d897cb2a
SIMON THOUET <simon.thouet@gmail.com>
2023-01-12 02:36:03 +0000

    Create calendar

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -1,6 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import DatePicker from "react-datepicker";
+
+import "./Booking.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 function Booking() {
   const carId = useParams();
@@ -19,6 +23,21 @@ function Booking() {
     { id: 1, start: "2023-01-22", end: "2023-01-30", car_id: 1, user_id: 3 },
   ]);
 
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [startDate, endDate] = dateRange;
+
+  // From calendar to component as an input: new Date(date);
+  // From component to calendar as an output: date.toISOString().substring(0, 10);
+
+  const handleBooking = (e) => {
+    e.preventDefault();
+    console.log(
+      startDate.toISOString().substring(0, 10),
+      " and ",
+      endDate.toISOString().substring(0, 10)
+    );
+  };
+
   return (
     <>
       <img src={car.image} alt="car_img" />
@@ -29,6 +48,23 @@ function Booking() {
       <p>Type : {car.type}</p>
       <br />
       <p>Price : {car.daily_price}$ per day</p>
+      <br />
+      <DatePicker
+        dateFormat="dd-MM-yyyy"
+        selectsRange={true}
+        startDate={startDate}
+        endDate={endDate}
+        onChange={(update) => {
+          setDateRange(update);
+        }}
+        excludeDateIntervals={books.map((book) => ({
+          start: new Date(book.start),
+          end: new Date(book.end),
+        }))}
+        placeholderText="Please select your dates"
+        withPortal
+      />
+      <button onClick={handleBooking}>Book now !</button>
     </>
   );
 }
