1e07a339ddc5a9a03292500b4ea190e0338cff00
André Macedo <asilvamacedo@gmail.com>
2023-01-12 18:04:19 +0000

    did some filtering in the display

--- src/components/DashboardComponent/DashboardComponent.jsx
+++ src/components/DashboardComponent/DashboardComponent.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-restricted-syntax */
 import * as React from "react";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import SearchIcon from "@mui/icons-material/Search";
+/* import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search"; */
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -13,25 +16,101 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import axios from "axios";
 import CarContext from "../../contexts/CarContext";
 
 import "./DashboardComponent.css";
 
-function DashboardComponent(props) {
-  const { cars } = useContext(CarContext);
+function DashboardComponent() {
+  const { cars, fetchCars } = useContext(CarContext);
   // eslint-disable-next-line react/destructuring-assignment
-  console.log(props.type);
+  /*   console.log(props.type); */
+  const [carName, setName] = useState();
+  const [type, setType] = useState();
+  const [CompanyName, setCompanyName] = useState();
+  const [max_price, setMax_price] = useState(500);
+  const [min_price, setMin_price] = useState(0);
+  const [allTypes, setAllTypes] = useState();
 
+  const fetchTypes = () => {
+    axios
+      .get(`http://localhost:5000/types/cars`)
+      .then((response) => {
+        setAllTypes(...[response.data]);
+      })
+      .catch((error) => {
+        // setUserNotFound(true);
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => fetchTypes, []);
+
+  const Obj = {
+    type,
+    CompanyName,
+    max_price,
+    min_price,
+    carName,
+  };
+  /*  console.log(Obj) */
+
+  useEffect(() => {
+    fetchCars(Obj);
+  }, [carName, min_price, max_price, type, CompanyName]);
+  const handleMin = (e) => {
+    setMin_price(e.target.value);
+  };
+  const handleMax = (e) => {
+    setMax_price(e.target.value);
+  };
+  const handleChange = (event) => {
+    setType(event.target.value);
+  };
   return (
     <div>
-      <h1>{props.type}</h1>
-      <Box
-        className="search-bar"
-        sx={{ display: "flex", alignItems: "flex-end" }}
-      >
-        <SearchIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
-        <TextField id="search-bar" label="Search" variant="standard" />
-      </Box>
+      <div className="filters">
+        <h2>Select some filters!</h2>
+        <form id="price-range-form">
+          <label htmlFor="min-price" className="form-label">
+            Min price: {`${min_price}`}
+          </label>
+          <input
+            type="range"
+            min="0"
+            max="300"
+            step="1"
+            value={`${min_price}`}
+            onInput={handleMin}
+          />
+          <label htmlFor="max-price" className="form-label">
+            Max price: {`${max_price}`}
+          </label>
+          <input
+            type="range"
+            className="form-range"
+            min="200"
+            max="500"
+            id="max-price"
+            step="1"
+            value={`${max_price}`}
+            onInput={handleMax}
+          />
+          <br />
+          <br />
+          <label htmlFor="dropdownBox"> </label>
+          <select name="dropdownBox" value={type} onChange={handleChange}>
+            <option value="All">Select a car type:</option>
+            {allTypes &&
+              allTypes.map((element, i) => (
+                <option value={element.type} key={i}>
+                  {element.type}
+                </option>
+              ))}
+          </select>
+        </form>
+      </div>
+      <br />
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
           {cars &&
@@ -46,14 +125,14 @@ function DashboardComponent(props) {
                       title={element.name}
                     />
                     <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {element.name}
+                      <Typography gutterBottom variant="h7" component="div">
+                        Car: {element.carName}
+                      </Typography>
+                      <Typography variant="body3" color="text.secondary">
+                        Price: {element.daily_price}
                       </Typography>
                       <Typography variant="body2" color="text.secondary">
-                        {element.daily_price}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {element.type}
+                        Car type: {element.type}
                       </Typography>
                     </CardContent>
                     <CardActions>
--- src/contexts/CarContext.jsx
+++ src/contexts/CarContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable react/jsx-no-constructed-context-values */
 /* eslint-disable react/prop-types */
 import { useState, useEffect, createContext } from "react";
@@ -9,9 +10,71 @@ export function CarContextProvider({ children }) {
   // creating the states for the variables
   const [cars, setCars] = useState([]);
 
-  const fetchCars = () => {
+  /* `http://localhost:5000/cars?name=${name}&type=${type}$&CompanyName=${CompanyName}$&max_price=${max_price}$&min_price=${min_price}$` */
+  const fetchCars = (object) => {
+    let query = `http://localhost:5000/cars`;
+    //   const { type, CompanyName, max_price, min_price, name } = object;
+    if (object?.CarName !== undefined) {
+      query += `?name=${object?.CarName}`;
+      if (object?.min_price !== undefined) {
+        query += `&min_price=${object?.min_price}`;
+      }
+      if (object?.max_price !== undefined) {
+        query += `&max_price=${object?.max_price}`;
+      }
+      if (object?.CompanyName !== undefined) {
+        query += `&CompanyName=${object?.CompanyName}`;
+      }
+      if (object?.type !== undefined && object?.type !== "All") {
+        query += `&type=${object?.type}`;
+      }
+    }
+    if (object?.min_price !== undefined && object?.CarName === undefined) {
+      query += `?min_price=${object?.min_price}`;
+
+      if (object?.max_price !== undefined) {
+        query += `&max_price=${object?.max_price}`;
+      }
+      if (object?.CompanyName !== undefined) {
+        query += `&CompanyName=${object?.CompanyName}`;
+      }
+      if (object?.type !== undefined && object?.type !== "All") {
+        query += `&type=${object?.type}`;
+      }
+    }
+    if (
+      object?.max_price !== undefined &&
+      object?.min_price === undefined &&
+      object?.CarName === undefined
+    ) {
+      query += `?max_price=${object?.max_price}`;
+      if (object?.CompanyName !== undefined) {
+        query += `&CompanyName=${object?.CompanyName}`;
+      }
+      if (object?.type !== undefined && object?.type !== "All") {
+        query += `&type=${object?.type}`;
+      }
+    }
+    if (
+      object?.CompanyName !== undefined &&
+      object?.max_price === undefined &&
+      object?.min_price === undefined &&
+      object?.CarName === undefined
+    ) {
+      query += `?CompanyName=${object?.CompanyName}`;
+    }
+    if (
+      object?.type !== undefined &&
+      object?.CompanyName === undefined &&
+      object?.max_price === undefined &&
+      object?.min_price === undefined &&
+      object?.CarName === undefined &&
+      object?.type !== "All"
+    ) {
+      query += `?type=${object?.type}`;
+    }
     axios
-      .get(`http://localhost:5000/cars`)
+      .get(query)
       .then((response) => {
         setCars(response.data);
       })
