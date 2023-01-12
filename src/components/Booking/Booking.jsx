7b3206aee2a3d6686931569f9bbcccbb66600991
SIMON THOUET <simon.thouet@gmail.com>
2023-01-12 00:10:47 +0000

    Create component and install module date picking

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Booking() {
   const carId = useParams();
-  console.log(carId.id);
-  return <div>Booking</div>;
+  const [car, setCar] = useState({});
+
+  const fetchCar = () => {
+    axios
+      .get("http://localhost:5000/cars")
+      .then((res) =>
+        setCar(res.data.find((e) => e.id === parseInt(carId.id, 10)))
+      );
+  };
+  useEffect(() => fetchCar(), []);
+
+  const [books, setBooks] = useState([
+    { id: 1, start: "2023-01-22", end: "2023-01-30", car_id: 1, user_id: 3 },
+  ]);
+
+  return (
+    <>
+      <img src={car.image} alt="car_img" />
+      <p>Company : {car.CompanyName}</p>
+      <br />
+      <p>Name : {car.carName}</p>
+      <br />
+      <p>Type : {car.type}</p>
+      <br />
+      <p>Price : {car.daily_price}$ per day</p>
+    </>
+  );
 }
 
 export default Booking;
