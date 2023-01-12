3f9a180cc98f2b4445494bf4a2a1a023b8f6bb2a
SIMON THOUET <simon.thouet@gmail.com>
2023-01-12 16:15:54 +0000

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { CarContextProvider } from "./contexts/CarContext";
+import { UserContextProvider } from "./contexts/UserContext";
 import DashboardPage from "./pages/DashboardPage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import Booking from "./components/Booking/Booking";
 import LandingPage from "./pages/LandingPage";
-import { CarContextProvider } from "./contexts/CarContext";
-import { UserContextProvider } from "./contexts/UserContext";
+// import AddCar from "./compenents/AddCar/AddCar";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             {/* TODO: testing route to remove after */}
             <Route path="/booking/:id" element={<Booking />} />
+            {/* <Route path="/booking/:id" element={<AddCar />} /> */}
           </Routes>
         </CarContextProvider>
       </UserContextProvider>
