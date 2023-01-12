d8a042d3d4d064723f50aff807487cd318c0a92d
SIMON THOUET <simon.thouet@gmail.com>
2023-01-12 22:08:31 +0000

    add book list comp

---
+++ src/components/BookingList/BookingList.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+function BookingList(props) {
+  const { userId, type } = props;
+  const [books, setBooks] = useState([]);
+  const 
+
+  useEffect(() => {
+    if (type==="company") {
+
+    }
+  }, []);
+
+  return <div>BookingList</div>;
+}
+
+export default BookingList;
